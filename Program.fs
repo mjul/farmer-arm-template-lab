866154ffdb59c27fc80e84035438a4ec3f507f9d
Martin Jul <martin@mjul.com>
2020-12-15 13:05:37 +0000

    Generate tutorial ARM template

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open Farmer
+open Farmer.Builders
+
+
+
+[<EntryPoint>]
+let main argv =
+
+    let myWebApp = webApp {
+        name "yourFirstFarmerApp"
+    }
+
+    let deployment = arm {
+        location Location.NorthEurope
+        add_resource myWebApp
+    }
+
+    deployment
+    |> Writer.quickWrite "myFirstTemplate"
+
+    0 // return an integer exit code
+
