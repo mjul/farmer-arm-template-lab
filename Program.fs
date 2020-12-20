7093d810e718d77adfdfb879f991f77076305670
Martin Jul <martin@mjul.com>
2020-12-20 14:08:36 +0000

    Take optional output file name from argv

--- Program.fs
+++ Program.fs
@@ -6,21 +6,26 @@ open Farmer
 open Farmer.Builders
 
 
-
 [<EntryPoint>]
 let main argv =
+    let outputFile = 
+        match argv with
+        | [| _ |] -> "myFirstTemplate"
+        | [| _; fname |] -> fname
 
     let myWebApp = webApp {
         name "yourFirstFarmerApp"
     }
 
     let deployment = arm {
-        location Location.NorthEurope
+        location Location.WestEurope
         add_resource myWebApp
     }
 
+    printfn "Writing ARM template to %s..." outputFile
+
     deployment
-    |> Writer.quickWrite "myFirstTemplate"
+    |> Writer.quickWrite outputFile
 
     0 // return an integer exit code
 
